3d7afa0dd7acf80a1adcbf70cd92514a90d44b04
jenny-c <chen.jenny525@gmail.com>
2016-06-16 09:48:44 -0400

    hardware webpage (html, css, js) and images

---
+++ hardwareProject/checkPassword.js
@@ -0,0 +1,8 @@
+const PASSWORD = "xeroxparc123"
+var userPassword = document.getElementById("passwordTextField")
+
+if (userPassword = PASSWORD) {
+  document.location.href = "file:///Users/jennychen/Dropbox/ICS2O7/ChenJ/HTMLDevelopment/hardwareProject/index.html"
+} else {
+  document.location.href = "file:///Users/jennychen/Dropbox/ICS2O7/ChenJ/HTMLDevelopment/hardwareProject/reportsLandingPage.html"
+}
